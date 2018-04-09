d82fd251d4c97739b31f51d7b53041940a2d75cd
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-09 19:32:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+# require 'pry'
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
+    open_line = []
+    katz_deli.each_with_index{|name, index| open_line.push("#{index + 1}. #{name}")}
+    open_line.join(" ")
+    puts "The line is currently: #{open_line.join(" ")}"
+    end
+end
+
