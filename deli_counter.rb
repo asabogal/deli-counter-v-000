8cf7f685ae4bd755600e7f984655c664f45d8dca
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-09 19:43:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def take_a_number(katz_deli, name)
   if katz_deli.size == 0
     katz_deli << name
     binding.pry
-    puts "Welcome, #{name}. You are number #{} in line.
+    puts "Welcome, #{name}. You are number #{} in line."
 
 end
 
