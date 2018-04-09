0aad561ddc6f562020762184a488f4d216a54c02
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-09 20:14:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,11 +10,12 @@ def line(katz_deli)
     puts "The line is currently: #{open_line.join(" ")}"
     end
 end
-
+binding.pry
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
+
 end
